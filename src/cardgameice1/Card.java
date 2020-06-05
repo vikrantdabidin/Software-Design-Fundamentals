07dcadff509f129d687f7da4fc7260ab08b9ef7b
vikrantdabidin <65800996+vikrantdabidin@users.noreply.github.com>
2020-06-05 18:24:42 -0400

    Update Card.java

--- src/cardgameice1/Card.java
+++ src/cardgameice1/Card.java
@@ -4,41 +4,40 @@
  * and open the template in the editor.
  */
 package cardgameice1;
-import java.util.Random;
 /**
  *
  * @author srinivsi
- * @modifier Abhishek Abhishek 
- * Student ID - 991587950
+ * @modifier Vikrant Dabidin - 991590150
  private variable then refactor it
  * - encapsulate fields (encapsulation principle) 
  getter/setters
  */
 public class Card {
     
-    private String suits;//diamonds,clubs,spades, hearts
+    private String suits; //diamonds,clubs,spades, hearts
+    
     private int value; // 1 to 13
+    
     public static final String[] SUITS = {"diamonds", "clubs", "spades", "hearts"};
-
-    public String getSuits() {
-        return suits;
+    
+    public String toString(){
+        return getValue() + " " + getSuits();
     }
 
-    public void setSuits(String suits) {
-        this.suits = suits;
+    public String getSuits() {
+        return this.suits;
+    }
+
+    public void setSuits() {
+        this.suits = SUITS[(int) (Math.random()*SUITS.length)];
     }
 
     public int getValue() {
-        return value;
+        return this.value;
     }
 
-    public void setValue(int value) {
-        this.value = value;
+    public void setValue() {
+        this.value = (int) (Math.random()*13 + 1);
     }
 
-    public int randomValue(int low, int high) {
-        Random r = new Random();
-        int value = r.nextInt(high - low) + low;
-        return value;
-    }
 }
